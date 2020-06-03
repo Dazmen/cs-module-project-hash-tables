9b368bb8e377eb7a27a5c94cd2fd66d5269bdeef
Chris Engel <Chris.Engel@yahoo.com>
2020-06-03 17:59:46 -0400

    word_count complete...

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,29 @@
+import re
+
+# Note: I think I despise regex/regular expression.....
+
 def word_count(s):
     # Your code here
+    word_cache = {}
 
+    hacky_removal = ['\t', '\r', '\n']
 
+    for annoying_regex in hacky_removal: # why does this work but a list of illegal chars threw me an error?
+        s = s.replace(annoying_regex, " ")
+
+    new_string = re.sub(r'[^a-zA-Z\'\s]', '', s)
+
+    words = new_string.split(' ')
+
+    for word in words:
+        if word.lower() not in word_cache and len(word) > 0:
+            word_cache[word.lower()] = 1
+        elif len(word) > 0:
+            word_cache[word.lower()] += 1
+
+    print('HELLO', word_cache)
+    return word_cache
+    
 
 if __name__ == "__main__":
     print(word_count(""))
