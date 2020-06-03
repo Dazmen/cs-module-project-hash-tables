44a82a10831dab4bc997628a17b3a071352bb50f
Chris Engel <Chris.Engel@yahoo.com>
2020-06-03 15:31:30 -0400

    look-up and expensive-sequence complete

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,16 @@
 # Your code here
 
+another_cache = {}
 
 def expensive_seq(x, y, z):
     # Your code here
-
-
+    if (x, y, z) not in another_cache:
+        if x <= 0:
+            another_cache[(x, y, z)] = y + z
+        else:
+            another_cache[(x, y, z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+            
+    return another_cache[(x, y, z)]
 
 if __name__ == "__main__":
     for i in range(10):
@@ -12,3 +18,8 @@ if __name__ == "__main__":
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
+
+# Planning
+
+## Same thing as last problem with the cache?
+## Create a cache were the keys are (x, y, z) since it is pretty much the fib/trib sequence again
