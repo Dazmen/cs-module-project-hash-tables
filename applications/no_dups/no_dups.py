fb0e745cd834dd1abde41b4f2c1c36dfdfba76f1
Chris Engel <Chris.Engel@yahoo.com>
2020-06-03 18:09:09 -0400

    no_dups complete

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,22 @@
 def no_dups(s):
     # Your code here
+    words = s.split(' ')
 
+    word_cache = {}
 
+    for word in words:
+        if word not in word_cache:
+            word_cache[word] = word
+    
+    new_words_list = []
+
+    for word in word_cache:
+        new_words_list.append(word)
+
+    new_string = " "
+
+    new_string = new_string.join(new_words_list)
+    return new_string
 
 if __name__ == "__main__":
     print(no_dups(""))
